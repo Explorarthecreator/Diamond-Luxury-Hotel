b783f76c2ac5d15eb036cfd3e7a5ac3ebf371463
explorarthecreator <fredadejiemma99@gmail.com>
2023-02-25 00:01:42 +0100

    Added functionality for the spaces section

--- index.js
+++ index.js
@@ -28,7 +28,7 @@
 //     $('#select-day')
 //         .append($("<option></option>")
 //         .attr("value", i + 1)
-//         .text(i + 1)); 
+//         .text(i + 1));
 //     }
 // }
 // else {
@@ -48,3 +48,182 @@
 //         .text(year + i)); 
 //     }
 // }
+
+const parking = document.getElementById('parking');
+const dining = document.getElementById('dining');
+const spa = document.getElementById('spa');
+const resort = document.getElementById('resort');
+const casino = document.getElementById('casino');
+const bedroom = document.getElementById('bedroom');
+const eventSpace = document.getElementById('event');
+
+
+
+const parkingPictures = document.getElementById('parkingPics')
+const mainPictures = document.querySelector('.spacepics')
+const spaPictures = document.getElementById('spaPics')
+const resortPics = document.getElementById('resortPics')
+const casinoPics = document.getElementById('casinoPics')
+const bedPics = document.getElementById('bedPics')
+const eventPics = document.getElementById('eventPics')
+
+
+const changeDining = ()=>{
+    mainPictures.setAttribute('style','display:block;');
+    parkingPictures.setAttribute('style','display:none;');
+    spaPictures.setAttribute('style','display:none;');
+    resortPics.setAttribute('style','display:none;');
+    casinoPics.setAttribute('style','display:none;');
+    bedPics.setAttribute('style','display:none;');
+    eventPics.setAttribute('style','display:none;');
+    dining.classList.add('acti')
+    parking.classList.remove(`acti`)
+    spa.classList.remove(`acti`)
+    resort.classList.remove(`acti`)
+    casino.classList.remove(`acti`)
+    bedroom.classList.remove(`acti`)
+    eventSpace.classList.remove(`acti`)
+    console.log(emma.innerHTML);
+};
+
+const changeParking = ()=>{
+    // alert('My NAme is Explorar')
+    // mainPictures.classList.add('hide')
+    // parkingPictures.classList.add('sho')
+    mainPictures.setAttribute('style','display:none;');
+    parkingPictures.setAttribute('style','display:block;');
+    spaPictures.setAttribute('style','display:none;');
+    resortPics.setAttribute('style','display:none;');
+    casinoPics.setAttribute('style','display:none;');
+    bedPics.setAttribute('style','display:none;');
+    eventPics.setAttribute('style','display:none;');
+    // emm.setAttribute('style','border-bottom:2px solid #583101')
+    parking.classList.add('acti')
+    dining.classList.remove(`acti`)
+    spa.classList.remove(`acti`)
+    resort.classList.remove(`acti`)
+    casino.classList.remove(`acti`)
+    bedroom.classList.remove(`acti`)
+    eventSpace.classList.remove(`acti`)
+    console.log(emma.innerHTML);
+};
+
+const changeSpa = ()=>{
+    // alert('My NAme is Explorar')
+    // emmma.setAttribute('style','display:none;');
+    // emma.setAttribute('style','display:block;');
+    // emm.setAttribute('style','border-bottom:2px solid #583101')
+    mainPictures.setAttribute('style','display:none;');
+    parkingPictures.setAttribute('style','display:none;');
+    spaPictures.setAttribute('style','display:block;');
+    resortPics.setAttribute('style','display:none;');
+    casinoPics.setAttribute('style','display:none;');
+    bedPics.setAttribute('style','display:none;');
+    eventPics.setAttribute('style','display:none;');
+    spa.classList.add('acti')
+    dining.classList.remove(`acti`)
+    parking.classList.remove(`acti`)
+    resort.classList.remove(`acti`)
+    casino.classList.remove(`acti`)
+    bedroom.classList.remove(`acti`)
+    eventSpace.classList.remove(`acti`)
+    console.log(emma.innerHTML);
+};
+
+const changeResort = ()=>{
+    // alert('My NAme is Explorar')
+    // emmma.setAttribute('style','display:none;');
+    // emma.setAttribute('style','display:block;');
+    // emm.setAttribute('style','border-bottom:2px solid #583101')
+    mainPictures.setAttribute('style','display:none;');
+    parkingPictures.setAttribute('style','display:none;');
+    spaPictures.setAttribute('style','display:none;');
+    resortPics.setAttribute('style','display:block;');
+    casinoPics.setAttribute('style','display:none;');
+    bedPics.setAttribute('style','display:none;');
+    eventPics.setAttribute('style','display:none;');
+    resort.classList.add('acti')
+    dining.classList.remove(`acti`)
+    spa.classList.remove(`acti`)
+    parking.classList.remove(`acti`)
+    casino.classList.remove(`acti`)
+    bedroom.classList.remove(`acti`)
+    eventSpace.classList.remove(`acti`)
+    console.log(emma.innerHTML);
+};
+
+const changeCasino = ()=>{
+    // alert('My NAme is Explorar')
+    // emmma.setAttribute('style','display:none;');
+    // emma.setAttribute('style','display:block;');
+    // emm.setAttribute('style','border-bottom:2px solid #583101')
+    mainPictures.setAttribute('style','display:none;');
+    parkingPictures.setAttribute('style','display:none;');
+    spaPictures.setAttribute('style','display:none;');
+    resortPics.setAttribute('style','display:none;');
+    casinoPics.setAttribute('style','display:block;');
+    bedPics.setAttribute('style','display:none;');
+    eventPics.setAttribute('style','display:none;');
+    casino.classList.add('acti')
+    dining.classList.remove(`acti`)
+    spa.classList.remove(`acti`)
+    resort.classList.remove(`acti`)
+    parking.classList.remove(`acti`)
+    bedroom.classList.remove(`acti`)
+    eventSpace.classList.remove(`acti`)
+    console.log(emma.innerHTML);
+};
+
+const changeBed = ()=>{
+    // alert('My NAme is Explorar')
+    // emmma.setAttribute('style','display:none;');
+    // emma.setAttribute('style','display:block;');
+    // emm.setAttribute('style','border-bottom:2px solid #583101')
+    mainPictures.setAttribute('style','display:none;');
+    parkingPictures.setAttribute('style','display:none;');
+    spaPictures.setAttribute('style','display:none;');
+    resortPics.setAttribute('style','display:none;');
+    casinoPics.setAttribute('style','display:none;');
+    bedPics.setAttribute('style','display:block;');
+    eventPics.setAttribute('style','display:none;');
+    bedroom.classList.add('acti')
+    dining.classList.remove(`acti`)
+    spa.classList.remove(`acti`)
+    resort.classList.remove(`acti`)
+    casino.classList.remove(`acti`)
+    parking.classList.remove(`acti`)
+    eventSpace.classList.remove(`acti`)
+    console.log(emma.innerHTML);
+};
+
+const changeEvents = ()=>{
+    // alert('My NAme is Explorar')
+    // emmma.setAttribute('style','display:none;');
+    // emma.setAttribute('style','display:block;');
+    // emm.setAttribute('style','border-bottom:2px solid #583101')
+    mainPictures.setAttribute('style','display:none;');
+    parkingPictures.setAttribute('style','display:none;');
+    spaPictures.setAttribute('style','display:none;');
+    resortPics.setAttribute('style','display:none;');
+    casinoPics.setAttribute('style','display:none;');
+    bedPics.setAttribute('style','display:none;');
+    eventPics.setAttribute('style','display:block;');
+    eventSpace.classList.add('acti')
+    dining.classList.remove(`acti`)
+    spa.classList.remove(`acti`)
+    resort.classList.remove(`acti`)
+    casino.classList.remove(`acti`)
+    bedroom.classList.remove(`acti`)
+    parking.classList.remove(`acti`)
+    console.log(emma.innerHTML);
+};
+
+
+// const reverse = ()=>{
+//     emmma.setAttribute('style','display:block;');
+//     emma.setAttribute('style','display:none')
+// }
+
+// add
+// remove
+// toggle
